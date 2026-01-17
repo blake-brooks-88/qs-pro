9161360d896608fe633a1a65dedc4714a479b6e4
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2026-01-17 13:04:41 -0600

    test(worker): use noop Redis client in tests

--- apps/worker/src/redis/redis.module.ts
+++ apps/worker/src/redis/redis.module.ts
@@ -2,17 +2,36 @@ import { Global, Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import Redis from "ioredis";
 
+function createNoopRedisClient() {
+  return {
+    publish: async () => 0,
+    set: async () => "OK",
+    get: async () => null,
+    incr: async () => 0,
+    decr: async () => 0,
+    expire: async () => 0,
+    duplicate: () => ({
+      subscribe: async () => undefined,
+      quit: async () => undefined,
+      on: () => undefined,
+      off: () => undefined,
+    }),
+  };
+}
+
 @Global()
 @Module({
   providers: [
     {
       provide: "REDIS_CLIENT",
       useFactory: (configService: ConfigService) => {
+        if (process.env.NODE_ENV === "test") {
+          return createNoopRedisClient();
+        }
+
         return new Redis(
           configService.get<string>("REDIS_URL", "redis://localhost:6379"),
-          {
-            maxRetriesPerRequest: null,
-          },
+          { maxRetriesPerRequest: null },
         );
       },
       inject: [ConfigService],
